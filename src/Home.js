2c221fbb765ccdb644878922b7bde50bc0eb99fd
Tarun-kumar-gupta999 <tarun22082@iiitd.ac.in>
2023-03-03 23:37:26 +0530

    3 march

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+    <div>
+        <p style={{marginBottom:'60px'}}></p>
+    </div>
+    <div style={{backgroundColor:'Grey'}}>
+        <div style={{width:'60%',marginLeft:'4%'}}>
+            <h1 className='multicolortext'>AI based consultancy and product development enterprise</h1>
+        </div>
+        <div>
+            <p style={{width:'60%',marginLeft:'4%',marginTop:'2%'}}>
+            We offer services to build deep technology-based products for various domains, including healthcare, agriculture, and retail. We develop products to enable data privacy and security in AI-based products. Machine Unlearning: We develop the product to unlearn data points from a machine Learning model. We offer conversational AI-based solutions to support or develop language model capabilities of various business entities.
+            </p>
+        </div>
+    </div>
+
+    
+    <div style={{marginLeft:'4%'}}>
+        <p style={{fontStyle:'bold'}}>Some more insight-</p>
+        <div className='c1'>
+        <div style={{borderStyle:'solid dotted dashed solid',width:'20%'}}>hiii</div>
+        <div>how</div>
+        <div>are</div>
+        <div>you</div>
+        </div>
+    </div>
+    </>
+  )
+}
